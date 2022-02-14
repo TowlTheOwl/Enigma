4fe70c7df0cb5a26f841ac8cd18498719aa1fca1
Henry Yang <88568778+ShroomOfDoomGoVroom@users.noreply.github.com>
2022-02-13 18:15:14 -0800

    Update possible-plugboard-connections.py

--- possible-plugboard-connections.py
+++ possible-plugboard-connections.py
@@ -1,27 +1,41 @@
-from itertools import combinations
-
-alphabet = [y for y in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+alphabet = [y for y in "ABCDEFG"]
 
 possible_pairs = [(a, b) for idx, a in enumerate(alphabet) for b in alphabet[idx + 1:]]
 
-total_combinations = []
-
-for i in range(1,14):
-    total_combinations.extend([j for j in combinations(possible_pairs, i)])
+print(possible_pairs)
+print(len(possible_pairs))
 
 res = []
-print(total_combinations)
-print(len(total_combinations))
-print(len(res))
-for k in range(len(total_combinations)):
-    comb = total_combinations[k]
-    used = []
-    letters = [y for a in comb for x in a for y in x]
-    for letter in letters:
-        if letters.count(letter) > 1:
-            break
-    else:
-        res.append(total_combinations[k])
+res.extend([[pair] for pair in possible_pairs])
 
-print(res)
-print(len(res))
+total_connections = []
+total_connections.extend(res)
+
+currentStep = total_connections
+iter = 0
+
+
+for num_conn in range(2, 14):
+    print(num_conn)
+    lastStep = currentStep
+    currentStep = []
+    for i in lastStep:
+        used = []
+        letters = [y for a in i for x in a for y in x]
+
+        used.extend(letters)
+        available = [(y, z) for y, z in possible_pairs if (y not in used) and (z not in used)]
+
+        for conn in available:
+            conns = i.copy()
+            conns.append(conn)
+            iter += 1
+
+            currentStep.append(conns)
+            if iter == 10000000:
+                print(conns)
+                iter = 0
+
+    total_connections.extend(currentStep)
+
+print(total_connections)
