e4cee3b98b12f373e06aa6429889268c849b6579
Henry Yang <88568778+ShroomOfDoomGoVroom@users.noreply.github.com>
2022-02-19 10:44:07 -0800

    Update possible-plugboard-connections.py

--- possible-plugboard-connections.py
+++ possible-plugboard-connections.py
@@ -1,4 +1,5 @@
 from itertools import permutations
+import time
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -13,43 +14,32 @@ currentStep = total_connections.copy()
 iter = 0
 
 num_conns = len(total_connections)
+print(1, num_conns)
 
 for num_conn in range(2, 10):
     num_conns = 0
-    lastStep = currentStep
+    lastStep = currentStep.copy()
     currentStep = []
+    perms = permutations(range(num_conn))
+    perms = list(perms)
     for i in lastStep:
         used = [y for a in i for y in a]
-        
         available = [(y, z) for y, z in possible_pairs if (y not in used) and (z not in used)]
-        perms = permutations(range(num_conn))
         for conn in available:
             conns = i.copy()
             conns.append(conn)
             for perm in perms:
-              a_list = [conns[i] for i in perm]
-              if a_list in currentStep:
-                break
+                a_list = [conns[i] for i in perm]
+                if a_list in currentStep:
+                    break
             else:
-              num_conns += 1
-              currentStep.append(conns)
-        #print(conns)
+                num_conns += 1
+                currentStep.append(conns)
 
-    #total_connections.extend(currentStep)
     with open('combinations.txt', 'a') as f:
-      for line in currentStep:
-        f.write(str(line) + '\n')
+        for line in currentStep:
+            f.write(str(line) + '\n')
 
-    x = 0
-    # for conn in currentStep:
-    #   print(conn, x)
-    #   perms = permutations(range(2))
-    #   for perm in perms:
-    #     a_list = [conn[i] for i in perm]
-    #     if a_list in currentStep:
-    #       x += 1
-    # print(x)
     print(num_conn, num_conns)
 
-
-#print(len(total_connections))
+# print(len(total_connections))
